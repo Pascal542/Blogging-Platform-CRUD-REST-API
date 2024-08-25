63209de7f08770560df7b907fdd97315cfc51ba8
Tsuna542 <aesc542@outlook.com>
2024-08-25 03:54:41 -0500

    Initialized DataBase and Entity

---
+++ src/main/java/com/pascal/api/blogger/entitiy/Blog.java
@@ -0,0 +1,30 @@
+package com.pascal.api.blogger.entitiy;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "blog_posts")
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "author", unique = true, nullable = false)
+    private String author;
+
+    @Column(name = "tags")
+    private List<String> tags;
+}
