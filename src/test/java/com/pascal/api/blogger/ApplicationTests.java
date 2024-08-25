56094b8164e308ffdad0e6c090ad53146974331a
Tsuna542 <aesc542@outlook.com>
2024-08-25 03:06:41 -0500

    First Commit

---
+++ src/test/java/com/pascal/api/blogger/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascal.api.blogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
